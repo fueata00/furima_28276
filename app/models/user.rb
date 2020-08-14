1711f22970dce884222f8b1603c935fc32c4a113
fueata00 <t.fujiwara1h@gmail.com>
2020-08-14 19:34:54 +0900

    正規表現を変数に入れて使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  valid_password_regex = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}\z/i
+  valid_name_regex = /\A[ぁ-んァ-ン一-龥]/
+  valid_name_kana_regex = /\A[ァ-ヶー－]+\z/
+
   with_options presence: true do
     validates :nickname
     validates :email
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}\z/i }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :password, format: { with: valid_password_regex }
+    validates :first_name, format: { with: valid_name_regex }
+    validates :last_name, format: { with: valid_name_regex }
+    validates :first_name_kana, format: { with: valid_name_kana_regex }
+    validates :last_name_kana, format: { with: valid_name_kana_regex }
     validates :birthday
   end
 end
