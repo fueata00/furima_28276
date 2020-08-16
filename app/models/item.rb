4f3a83b61512c2c768f03491022408232339cfb2
fueata00 <t.fujiwara1h@gmail.com>
2020-08-16 19:24:29 +0900

    itemsテーブルのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,19 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_fee
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_time
-  belongs_to :user
   has_one_attached :image
+  belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 }
+    validates :shipping_fee_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :shipping_time_id, numericality: { other_than: 1 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :user
+    validates :image
+  end
 end
