51da785a3cbc5383bc61e0b316c2d6a5cc843f25
fueata00 <t.fujiwara1h@gmail.com>
2020-08-16 22:40:21 +0900

    商品出品画面の作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,7 @@ class Item < ApplicationRecord
     validates :shipping_fee_id, numericality: { other_than: 1 }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :shipping_time_id, numericality: { other_than: 1 }
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :user
     validates :image
   end
