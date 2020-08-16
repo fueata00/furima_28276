17c7eef24c6fd4122cd9a8fb098783dabab811a3
fueata00 <t.fujiwara1h@gmail.com>
2020-08-16 18:06:30 +0900

    選択肢用モデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_time
   belongs_to :user
 end
