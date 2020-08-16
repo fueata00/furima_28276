761a3a6da76e35aedbffc8a9d444cb221d1aa7fe
fueata00 <t.fujiwara1h@gmail.com>
2020-08-16 17:31:17 +0900

    itemsテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   valid_password_regex = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}\z/i
   valid_name_regex = /\A[ぁ-んァ-ン一-龥]/
   valid_name_kana_regex = /\A[ァ-ヶー－]+\z/
