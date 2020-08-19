467aa920207d44aaf4e6df1bc4188bb85a5e36b0
fueata00 <t.fujiwara1h@gmail.com>
2020-08-19 17:16:21 +0900

    コード整形

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,9 +35,7 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if current_user.id == item.user_id
-      item.destroy
-    end
+    item.destroy if current_user.id == item.user_id
     redirect_to root_path
   end
 
