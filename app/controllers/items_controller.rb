e6e60cc314557edbec0d6c752bb97aad8265a52c
fueata00 <t.fujiwara1h@gmail.com>
2020-08-19 16:03:10 +0900

    エラーメッセージ表示のためupdateアクションを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
   
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -28,11 +28,10 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
-      redirect_to item_path(item)
+    if @item.update(item_params)
+      redirect_to item_path(@item)
     else
-      redirect_to edit_item_path(item)
+      render :edit
     end
   end
 
