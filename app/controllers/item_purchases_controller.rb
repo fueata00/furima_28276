542657817c2a420cf04040663ed76341fd151003
fueata00 <t.fujiwara1h@gmail.com>
2020-08-20 18:42:12 +0900

    商品購入ページで商品情報の表示

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -1,5 +1,10 @@
 class ItemPurchasesController < ApplicationController
   def index
+    begin
+      @item = Item.find(params[:item_id])
+    rescue
+      redirect_to root_path
+    end
   end
 
   def create
