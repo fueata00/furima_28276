c9e4b129b3b1b9f938c0e98c7a8859dee47ae6a0
fueata00 <t.fujiwara1h@gmail.com>
2020-08-21 19:00:05 +0900

    itemsテーブルへ購入フラグ用のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_081648) do
+ActiveRecord::Schema.define(version: 2020_08_21_093029) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_081648) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "sold_out", default: false, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
