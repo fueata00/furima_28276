737a66e54378be92559da9dad11daf6c54badc39
fueata00 <t.fujiwara1h@gmail.com>
2020-08-21 19:18:13 +0900

    カラム名の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,7 +18,7 @@ class Item < ApplicationRecord
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :shipping_time_id, numericality: { other_than: 1 }
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-    validates :user
+    validates :user_id
     validates :image
   end
   validates :sold_out, inclusion: { in: [true, false] }
