b766a469fddbf2b9ea0913d1127fad895f931b15
fueata00 <t.fujiwara1h@gmail.com>
2020-09-10 22:18:38 +0900

    purchase_infoフォームモデルの単体テストコード修正

--- spec/models/purchase_info_spec.rb
+++ spec/models/purchase_info_spec.rb
@@ -12,52 +12,52 @@ RSpec.describe PurchaseInfo, type: :model do
     it 'クレジットカードのトークンが取得できなければ保存できないこと' do
       @purchase_info.token = nil
       @purchase_info.valid?
-      expect(@purchase_info.errors.full_messages).to include("Token can't be blank")
+      expect(@purchase_info.errors.full_messages).to include("クレジットカード情報を入力してください")
     end
     it 'postal_codeが空だと保存できないこと' do
       @purchase_info.postal_code = nil
       @purchase_info.valid?
-      expect(@purchase_info.errors.full_messages).to include("Postal code can't be blank")
+      expect(@purchase_info.errors.full_messages).to include("郵便番号を入力してください")
     end
     it 'postal_codeにハイフンが含まれないと保存出来ないこと' do
       @purchase_info.postal_code = '1234567'
       @purchase_info.valid?
-      expect(@purchase_info.errors.full_messages).to include('Postal code is invalid')
+      expect(@purchase_info.errors.full_messages).to include('郵便番号は不正な値です')
     end
     it 'prefecture_idが1(---)だと保存出来ないこと' do
       @purchase_info.prefecture_id = 1
       @purchase_info.valid?
-      expect(@purchase_info.errors.full_messages).to include('Prefecture must be other than 1')
+      expect(@purchase_info.errors.full_messages).to include('都道府県は1以外の値にしてください')
     end
     it 'cityが空だと保存出来ないこと' do
       @purchase_info.city = nil
       @purchase_info.valid?
-      expect(@purchase_info.errors.full_messages).to include("City can't be blank")
+      expect(@purchase_info.errors.full_messages).to include("市区町村を入力してください")
     end
     it 'addressが空だと保存出来ないこと' do
       @purchase_info.address = nil
       @purchase_info.valid?
-      expect(@purchase_info.errors.full_messages).to include("Address can't be blank")
+      expect(@purchase_info.errors.full_messages).to include("番地を入力してください")
     end
     it 'phone_numberが空だと保存出来ないこと' do
       @purchase_info.phone_number = nil
       @purchase_info.valid?
-      expect(@purchase_info.errors.full_messages).to include("Phone number can't be blank")
+      expect(@purchase_info.errors.full_messages).to include("電話番号を入力してください")
     end
     it 'phone_numberが9桁以下だと保存出来ないこと' do
       @purchase_info.phone_number = '111111111'
       @purchase_info.valid?
-      expect(@purchase_info.errors.full_messages).to include('Phone number is invalid')
+      expect(@purchase_info.errors.full_messages).to include('電話番号は不正な値です')
     end
     it 'phone_numberが12桁以上だと保存出来ないこと' do
       @purchase_info.phone_number = '111111111111'
       @purchase_info.valid?
-      expect(@purchase_info.errors.full_messages).to include('Phone number is invalid')
+      expect(@purchase_info.errors.full_messages).to include('電話番号は不正な値です')
     end
     it 'phone_numberに数字以外が含まれると保存出来ないこと' do
       @purchase_info.phone_number = '111abc1111'
       @purchase_info.valid?
-      expect(@purchase_info.errors.full_messages).to include('Phone number is invalid')
+      expect(@purchase_info.errors.full_messages).to include('電話番号は不正な値です')
     end
   end
 end
