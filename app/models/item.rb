9221301b8626fa3886d60a99574daf27cdd09fb3
fueata00 <t.fujiwara1h@gmail.com>
2020-09-10 22:39:13 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,8 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_one :item_purchase
+  has_many :item_tag_relations
+  has_many :tags, through: :item_tag_relations
 
   with_options presence: true do
     validates :name
