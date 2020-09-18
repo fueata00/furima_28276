532207dc98d9744037c92cd7b35e7e0030966e3c
fueata00 <t.fujiwara1h@gmail.com>
2020-09-18 17:15:30 +0900

    インクリメンタルサーチ用のルーティング及びアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,6 +51,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def search
+    return nil if params[:input] == ""
+    tag = Tag.where(['name LIKE ?', "%#{params[:input]}%"] )
+    render json:{ keyword: tag }
+  end
+
   private
 
   def item_params
