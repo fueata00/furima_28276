78b89bdf50c3e0faf03789a61faf8b01cae005a0
fueata00 <t.fujiwara1h@gmail.com>
2020-09-20 21:09:44 +0900

    詳細検索の価格が選択に合わせて自動入力されるようJavaScriptの作成

---
+++ app/javascript/search.js
@@ -0,0 +1,48 @@
+function searchPriceInfo() {
+  if (document.getElementById("search-price-select") != null) {
+    const priceSelect = document.getElementById('search-price-select')
+    const inputPriceMax = document.getElementById("search-price-max");
+    const inputPriceMin = document.getElementById("search-price-min");
+    
+    priceSelect.addEventListener("input", () => {
+      let {min,max} = priceSelectJudge(priceSelect)
+      inputPriceMax.value = max;
+      inputPriceMin.value = min;
+    });
+  };
+}
+
+function priceSelectJudge(psj){
+  let min = null;
+  let max = null;
+  switch (psj.value){
+    case '1':
+      break;
+    case '2':
+      min = 300;
+      max = 1000;
+      break;
+    case '3':
+      min = 1000;
+      max = 5000;
+      break;
+    case '4':
+      min = 5000;
+      max = 10000;
+      break;
+    case '5':
+      min = 10000;
+      max = 30000;
+      break;
+    case '6':
+      min = 30000;
+      max = 50000;
+      break;
+    case '7':
+      min = 50000;
+      break;
+  }
+  return {min,max};
+}
+
+window.addEventListener("load", searchPriceInfo);
